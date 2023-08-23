6d9140895bcadf681c7a739b9edc5c462d9ae025
Sraddha-aa <sraddha@mozilor.com>
2023-08-23 11:55:19 +0530

    final

---
+++ gatsby-material-starter/gatsby-ssr.js
@@ -0,0 +1,11 @@
+
+   import React from 'react';
+export function onRenderBody({ setHeadComponents }) {
+setHeadComponents([
+    <script
+      id='cookieyes'
+      type='text/javascript'
+      src='https://cdn-cookieyes.com/client_data/3c8c2f951e08cada9b24688c/script.js'
+    ></script>,
+ ]);
+}
